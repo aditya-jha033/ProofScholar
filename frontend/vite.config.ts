dd8204e8073a27d223cc2d6c7069a557d2ae7269
aditya-jha033 <jha073803@gmail.com>
2026-08-01 23:33:00 +0530

    feat: update dependencies components

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,7 +2,13 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import topLevelAwait from 'vite-plugin-top-level-await';
 import wasm from 'vite-plugin-wasm';
+import path from "path";
 
 export default defineConfig({
   plugins: [react(), wasm(), topLevelAwait()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
